12af9a09ca2a53049af35d7607d50a32856f73b2
simon-luo <342699843@qq.com>
2016-04-01 14:24:51 +0800

    edit

--- app/src/main/java/com/simonluo/daidai_weather/utils/RetrofitSingleton.java
+++ app/src/main/java/com/simonluo/daidai_weather/utils/RetrofitSingleton.java
@@ -1,6 +1,10 @@
 package com.simonluo.daidai_weather.utils;
 
 import android.content.Context;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -12,6 +16,7 @@ import java.util.concurrent.Executors;
 
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
+import retrofit.RxJavaCallAdapterFactory;
 
 /**
  * Created by 333 on 2016/3/22.
@@ -30,9 +35,37 @@ public class RetrofitSingleton {
     public static void init(final Context mContext){
         Executor executor = Executors.newCachedThreadPool();
         Gson gson = new GsonBuilder().create();
-//        retrofit = new Retrofit.Builder().addCallAdapterFactory(GsonConverterFactory.create(gson))
-//                .baseUrl(ApiInterface.API)
-//                .callbackExecutor(executor)
-//                .addCallAdapterFactory(Rx)
+        retrofit = new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create(gson))
+                .baseUrl(ApiInterface.API)
+                .callbackExecutor(executor)
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+        apiService = retrofit.create(ApiInterface.class);
+    }
+
+    public static ApiInterface getApiService(Context context){
+        if (apiService != null){
+            return apiService;
+        }
+        init(context);
+        return getApiService(context);
+    }
+
+    public static Retrofit getRetrofit(Context context){
+        if (retrofit != null){
+            return retrofit;
+        }
+        init(context);
+        return getRetrofit(context);
+    }
+
+    public static void disposeFailureInfo(Throwable t, Context context, View view){
+        if (t.toString().contains("GaiException") || t.toString().contains("SocketTimeoutException") ||
+                t.toString().contains("UnknownHostException")) {
+            Snackbar.make(view, "网络不好", Snackbar.LENGTH_LONG).show();
+        }else {
+            Toast.makeText(context, t.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        Log.w(TAG, t.toString());
     }
 }
