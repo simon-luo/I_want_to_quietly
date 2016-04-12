8dfe7d5de447e1cfcb76f11cf2492883cab0caa0
simon-luo <342699843@qq.com>
2016-04-12 17:14:52 +0800

    edit

--- app/src/main/java/com/simonluo/daidai_weather/base/BaseActivity.java
+++ app/src/main/java/com/simonluo/daidai_weather/base/BaseActivity.java
@@ -1,7 +1,20 @@
 package com.simonluo.daidai_weather.base;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.simonluo.daidai_weather.utils.ACache;
+import com.simonluo.daidai_weather.utils.Constant;
+
 /**
  * Created by 333 on 2016/3/22.
  */
-public class BaseActivity {
+public class BaseActivity extends AppCompatActivity{
+    public ACache aCache;
+    public Constant mConstant = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
