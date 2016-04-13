e63131bbd254a3d2bc66ed0ddf943b1c5c97c6b5
simon-luo <342699843@qq.com>
2016-04-13 17:12:26 +0800

    edit

--- app/src/main/java/com/simonluo/daidai_weather/activity/WeatherActivity.java
+++ app/src/main/java/com/simonluo/daidai_weather/activity/WeatherActivity.java
@@ -1,7 +1,9 @@
 package com.simonluo.daidai_weather.activity;
 
+import com.simonluo.daidai_weather.base.BaseActivity;
+
 /**
  * Created by 333 on 2016/3/16.
  */
-public class WeatherActivity {
+public class WeatherActivity extends BaseActivity{
 }
