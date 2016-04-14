047a9a8769a52c646742082860d294d08325012e
simon-luo <342699843@qq.com>
2016-04-14 17:15:18 +0800

    edit

---
+++ app/src/main/java/com/simonluo/daidai_weather/activity/ChooseCityActivity.java
@@ -0,0 +1,17 @@
+package com.simonluo.daidai_weather.activity;
+
+import android.support.v4.widget.ContentLoadingProgressBar;
+import android.support.v7.widget.RecyclerView;
+
+import com.simonluo.daidai_weather.base.BaseActivity;
+
+/**
+ * Created by 333 on 2016/4/14.
+ */
+public class ChooseCityActivity extends BaseActivity{
+    private static String TAG = ChooseCityActivity.class.getSimpleName();
+
+    private RecyclerView mRecyclerView;
+    private ContentLoadingProgressBar mProgressBar;
+
+}
---
+++ app/src/main/java/com/simonluo/daidai_weather/utils/CityDBManager.java
@@ -0,0 +1,9 @@
+package com.simonluo.daidai_weather.utils;
+
+/**
+ * 城市数据库管理类
+ * Created by 333 on 2016/4/14.
+ */
+public class CityDBManager {
+
+}
