1f3df00b26fa8d477e52d8760aa6f6fcfb91f794
simon-luo <342699843@qq.com>
2016-04-15 17:03:57 +0800

    edit

--- app/src/main/java/com/simonluo/daidai_weather/activity/ChooseCityActivity.java
+++ app/src/main/java/com/simonluo/daidai_weather/activity/ChooseCityActivity.java
@@ -4,6 +4,13 @@ import android.support.v4.widget.ContentLoadingProgressBar;
 import android.support.v7.widget.RecyclerView;
 
 import com.simonluo.daidai_weather.base.BaseActivity;
+import com.simonluo.daidai_weather.entity.CityEntity;
+import com.simonluo.daidai_weather.entity.ProvinceEntity;
+import com.simonluo.daidai_weather.utils.CityDB;
+import com.simonluo.daidai_weather.utils.CityDBManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 333 on 2016/4/14.
@@ -13,5 +20,12 @@ public class ChooseCityActivity extends BaseActivity{
 
     private RecyclerView mRecyclerView;
     private ContentLoadingProgressBar mProgressBar;
+    private CityDBManager mCityDBManager;
+    private CityDB mCityDB;
+    private ArrayList<String> dataList = new ArrayList<>();
+    private ProvinceEntity selectProvince;
+    private CityEntity selectCity;
+    private List<ProvinceEntity> provinceList;
+    private List<CityEntity> cityList;
 
 }
---
+++ app/src/main/java/com/simonluo/daidai_weather/adapter/CityAdapter.java
@@ -0,0 +1,7 @@
+package com.simonluo.daidai_weather.adapter;
+
+/**
+ * Created by 333 on 2016/4/15.
+ */
+public class CityAdapter {
+}
